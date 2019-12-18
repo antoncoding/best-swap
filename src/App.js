d081e2b7646c7df9daefc678c9d3aeba67c7c9fb
Anton Cheng <anton@coolbitx.com>
2019-12-18 14:28:47 +0800

    Update syntax

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React, { useState } from 'react'
 
 import ChangellyEx from './Componenets/Changelly'
-import { Main, Header, Button, IconPlus, SidePanel, Box} from '@aragon/ui'
+import { Main, Header, Button, IconPlus, SidePanel, Box } from '@aragon/ui'
 import AragonUILogog from './aragonui-logo.png'
 import ChangellyLogo from './changelly-logo.png'
 import './App.css'
 
 function App() {
-  const [
-    sidePanelOpened,    
-    setSidePanelOpened
-  ] = useState(false)
+  const [sidePanelOpened, setSidePanelOpened] = useState(false)
 
   return (
     <Main>
       <Header
-        primary={
-          <>
-          </>
-        }
+        primary={<></>}
         secondary={
           <Button
             // mode='strong'
@@ -33,18 +27,14 @@ function App() {
 
       <ChangellyEx />
 
-      <SidePanel title="About BestSwap" 
-      opened={sidePanelOpened}
-      onClose={()=>setSidePanelOpened(false)}
-      >
-        <div style={{padding: 20, fontSize: 15}}> Partners </div>
+      <SidePanel title='About BestSwap' opened={sidePanelOpened} onClose={() => setSidePanelOpened(false)}>
+        <div style={{ padding: 20, fontSize: 15 }}> Powered By </div>
         <Box>
-          <img  width={300} src={AragonUILogog} alt='aragon-ui'></img>
+          <img width={300} src={AragonUILogog} alt='aragon-ui'></img>
         </Box>
         <Box>
-          <img  width={300} src={ChangellyLogo} alt='aragon-ui'></img>
+          <img width={300} src={ChangellyLogo} alt='aragon-ui'></img>
         </Box>
-         
       </SidePanel>
     </Main>
   )
