8638e3ad43bfa825a0a0fde6eac66392fb4a5532
antoncoding <antonassocareer@gmail.com>
2019-12-22 11:34:19 +0000

    Deploying to gh-pages from  cb0495758854c1a1ac54556e0473231ec88969a2

---
+++ precache-manifest.ea57816139749b4e12e834c1c57f65cf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e17095eb1638c6fedaa740f19f4467de",
+    "url": "/best-swap/index.html"
+  },
+  {
+    "revision": "55f45fb22f4f70fe15dc",
+    "url": "/best-swap/static/css/main.1d59d1eb.chunk.css"
+  },
+  {
+    "revision": "8907d676a8a326a0e037",
+    "url": "/best-swap/static/js/2.82ba93d2.chunk.js"
+  },
+  {
+    "revision": "52f5d968f652eaa0451957b3e57aeba9",
+    "url": "/best-swap/static/js/2.82ba93d2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "55f45fb22f4f70fe15dc",
+    "url": "/best-swap/static/js/main.5fa28c5d.chunk.js"
+  },
+  {
+    "revision": "f4ada9b174622ade6b45",
+    "url": "/best-swap/static/js/runtime-main.5d0cba9d.js"
+  },
+  {
+    "revision": "c72415d5f7d3239ff2baf348cb0606f1",
+    "url": "/best-swap/static/media/aragonui-logo.c72415d5.png"
+  },
+  {
+    "revision": "1cd0e57536864cd8ad77859333b6f9f2",
+    "url": "/best-swap/static/media/changelly-logo.1cd0e575.png"
+  }
+]);
